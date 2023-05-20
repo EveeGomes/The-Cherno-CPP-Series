59d002916f222157ef37409183d631366c743f4b
EvelineGomes <gomes.evepg@gmail.com>
2023-05-20 12:41:50 -0400

    overloading shift left operator

--- copy-copy-constructor/main.cpp
+++ copy-copy-constructor/main.cpp
@@ -7,18 +7,22 @@ private:
    char* m_Buffer;
    unsigned int m_Size;
 public:
-   // The constructor will take a const char that we'll call string:
    String(const char* string)
    {
-      // we need the size of the string to allocate enough memory and copy the content to m_Buffer
       m_Size = strlen(string);
-      m_Buffer = new char[m_Size]; // here we need [m_Size + 1] to give space for the null terminator character but for now it's left without it to test what happens
+      m_Buffer = new char[m_Size];
       
-      // now we can copy what's in the string pointer into the m_Buffer pointer
-      // this could be done using a for loop going through each char element but we can use memcpy:
-      memcpy(m_Buffer, string, m_Size); // the size is in bytes! We use m_Size because we're dealing with char
+      memcpy(m_Buffer, string, m_Size);
    }
 };
+
+// To be able to print an object of String class, we overload the "print" operator (shift left <<)
+std::ostream& operator<<(std::ostream& stream, const String& string)
+{
+   // first we call a method to put information into stream and then we return stream
+   stream << string.GetBuffer();
+   return stream;
+}
 
 int main()
 {
