6204e81a9e118a7b269fd4b8217d64ac61b0f6e2
EvelineGomes <gomes.evepg@gmail.com>
2023-05-20 14:41:45 -0400

    working on copy contructor (now showing default shallow copy)

--- copy-copy-constructor/main.cpp
+++ copy-copy-constructor/main.cpp
@@ -15,6 +15,13 @@ public:
       m_Buffer[m_Size] = 0;
    }
 
+   // Copy constructor takes as arg a reference to an object of the same class
+   String(const String& other)
+      :m_Buffer(other.m_Buffer), m_Size(other.m_Size) // this is what C++ already provides in case we don't define a copy constructor. This is just a shallow copy!
+   {
+
+   }
+
    ~String()
    {
       delete[] m_Buffer;
@@ -22,7 +29,6 @@ public:
 
    char& operator[](unsigned int index)
    {
-      // we won't do validation/safety check to see if it's inside the bounds...
       return m_Buffer[index];
    }
 
@@ -40,12 +46,7 @@ int main()
    String string = "Cherno";
    String second = string;
 
-   // to try this:
    second[2] = 'a';
-   // we need to overload the [] operator
-   // this operator needs to return a char reference in order to be set to a value we want (in this case a)
-   // in fact when we use like that it'll call the overloaded function we'll write that will do the operations we need here, which is set a char value to another value we're passing
-   // so the overloaded [] operator function will accept an integer and return a character in the m_Buffer string at the location represented by the integer!
 
    std::cout << string << std::endl;
    std::cout << second << std::endl;
