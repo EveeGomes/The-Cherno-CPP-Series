64baaf1a3c9237127880fcbb189494932964834a
EvelineGomes <gomes.evepg@gmail.com>
2023-05-20 12:37:20 -0400

    working on constructor

--- copy-copy-constructor/main.cpp
+++ copy-copy-constructor/main.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <string>
 
-struct Vector2
+class String
 {
-   float x, y;
+private:
+   char* m_Buffer;
+   unsigned int m_Size;
+public:
+   // The constructor will take a const char that we'll call string:
+   String(const char* string)
+   {
+      // we need the size of the string to allocate enough memory and copy the content to m_Buffer
+      m_Size = strlen(string);
+      m_Buffer = new char[m_Size]; // here we need [m_Size + 1] to give space for the null terminator character but for now it's left without it to test what happens
+      
+      // now we can copy what's in the string pointer into the m_Buffer pointer
+      // this could be done using a for loop going through each char element but we can use memcpy:
+      memcpy(m_Buffer, string, m_Size); // the size is in bytes! We use m_Size because we're dealing with char
+   }
 };
 
 int main()
 {
-   // do heap allocation with new keyword
-   Vector2* a = new Vector2();
-   // now the below code is copying the pointer instead of the value, so it's a different thing!
-   Vector2* b = a;
-
-   // so if we access b and change any value there, it'll also affect a because they're pointers pointing to the same address:
-   b->x = 2; // will affect a.x value as well!
 
    std::cin.get();
 }
