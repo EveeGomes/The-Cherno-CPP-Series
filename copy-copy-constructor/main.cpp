34f63c0f025810b399b65edcdeeab5bccadfb35f
EvelineGomes <gomes.evepg@gmail.com>
2023-05-20 11:07:13 -0400

    in heap allocation and accessing + changing value w/ a pointer

--- copy-copy-constructor/main.cpp
+++ copy-copy-constructor/main.cpp
@@ -8,9 +8,13 @@ struct Vector2
 
 int main()
 {
-   Vector2 a = { 2, 3 };
-   Vector2 b = a;
-   b.x = 5;
+   // do heap allocation with new keyword
+   Vector2* a = new Vector2();
+   // now the below code is copying the pointer instead of the value, so it's a different thing!
+   Vector2* b = a;
 
-   std::cin.get(); // the program waits us to press enter to close :)
+   // so if we access b and change any value there, it'll also affect a because they're pointers pointing to the same address:
+   b->x = 2; // will affect a.x value as well!
+
+   std::cin.get();
 }
