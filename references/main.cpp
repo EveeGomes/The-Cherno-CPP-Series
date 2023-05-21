dc6b19e065037d8e3153be494341b0c0fc155041
EvelineGomes <gomes.evepg@gmail.com>
2023-05-21 09:57:56 -0400

    increment() using pointer as param

--- references/main.cpp
+++ references/main.cpp
@@ -2,17 +2,23 @@
 
 #define LOG(x) std::cout << x << std::endl;
 
-void increment(int value)
+// So instead of changing the copy (and end up with the real variable unchanged), we need to access what's stored in the specific address and made a change there! 
+// ie. we need the address to be passed as arg of the function to perfom the real change.
+// so we gotta work with a pointer (since it holds addresses):
+void increment(int* value) // the parameter is a pointer; when calling this function the argument passed will be an address of the variable we want
 {
-   // passing by value is the same as the creating a copy of the value inside the function and using this copy instead of the variable passed
-   // int value = 5;
-   value++;
+   // we'll need to dereference the pointer; this is done by the * operator before the pointer identifier: *ptr
+   // to dereference means we'll get the value from the address pointed by the pointer
+   // if we do: *value++; due to the order of operations the increment will happen first, meaning we would be incrementing the POINTER
+   // so to avoid that and actually increment the value, we'll use () to first dereference the pointer and then with the value return by that dereference we perfom the increment operation!
+   (*value)++;
 }
 
 int main()
 {
    int a = 5;
-   increment(a);
+   // and here, we'll gotta pass the address of a variable to increment():
+   increment(&a);
    LOG(a);
 
 }
