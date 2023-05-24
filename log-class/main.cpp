78c48bed42afdb31900b84e5464cb63f42ac0c46
EvelineGomes <gomes.evepg@gmail.com>
2023-05-23 21:32:24 -0400

    set a private member that maintain the level of the log

--- log-class/main.cpp
+++ log-class/main.cpp
@@ -2,10 +2,14 @@
 
 class Log
 {
+private:
+   // this member variable will maintain what level the log is set
+   int m_LogLevel;
+
 public:
    void SetLevel(int level)
    {
-
+      m_LogLevel = level;
    }
 
    void Warn(const char* message)
