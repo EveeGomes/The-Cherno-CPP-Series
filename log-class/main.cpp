55a5915fab0468ae03ef9aea9cd6778d0bf17b88
EvelineGomes <gomes.evepg@gmail.com>
2023-05-23 21:28:37 -0400

    declaring public functions (methods)

--- log-class/main.cpp
+++ log-class/main.cpp
@@ -2,19 +2,22 @@
 
 class Log
 {
+public:
+   void SetLevel(int level)
+   {
 
+   }
+
+   void Warn(const char* message)
+   {
+
+   }
 };
 
 int main()
 {
-   // The best way to start writing a class is to think of its usage! What you need this class to do?
-   // try making some examples in main function on how we'd might use this class!!
-
-   // 1. instantiated
    Log log;
-   // 2. set a log level
-   log.SetLevel(LogLevelWarning); // warnings or errors, not tracing messages!
-   // 3. log a warn
+   log.SetLevel(LogLevelWarning);
    log.Warn("Hello!");
 
    std::cin.get();
