30599b4791bf817bed95a6754203f13210ec2c54
EvelineGomes <gomes.evepg@gmail.com>
2023-05-23 21:49:30 -0400

    trying things and adding validation in methods

--- log-class/main.cpp
+++ log-class/main.cpp
@@ -3,13 +3,12 @@
 class Log
 {
 public:
-   // these variables help us on not having to memorize what each 0, 1, 2 mean!
    const int LogLevelError = 0;
    const int LogLevelWarning = 1;
    const int LogLevelInfo = 2;
 
 private:
-   int m_LogLevel = LogLevelInfo; // by default everything should get printed
+   int m_LogLevel = LogLevelInfo;
 
 public:
    void SetLevel(int level)
@@ -17,17 +16,35 @@ public:
       m_LogLevel = level;
    }
 
+   // add in each of these methods an if statement to validate whether the message should be printed!
+   // We gotta check if m_LogLevel is set to the appropriate log level of that particular method.
+
+   void Error(const char* message)
+   {
+      if(m_LogLevel >= LogLevelError) 
+         std::cout << "[ERROR]: " << message << std::endl;
+   }
+
    void Warn(const char* message)
    {
-      
+      if (m_LogLevel >= LogLevelWarning)
+         std::cout << "[WARNING]: " << message << std::endl;
+   }
+
+   void Info(const char* message)
+   {
+      if (m_LogLevel >= LogLevelInfo)
+         std::cout << "[INFO]: " << message << std::endl;
    }
 };
 
 int main()
 {
    Log log;
-   log.SetLevel(LogLevelWarning);
+   log.SetLevel(log.LogLevelWarning);
    log.Warn("Hello!");
+   log.Error("Hello!");
+   log.Info("Hello!");
 
    std::cin.get();
 }
